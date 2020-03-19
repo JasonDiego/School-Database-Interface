3040ad8080f58d273f9af35fab72f70f85097652
JasonAg <jason.aguilera.s@gmail.com>
2020-03-18 21:47:36 -0400

    Pushed Runner.java to allow user to select data source type

---
+++ Runner.java
@@ -0,0 +1,57 @@
+package schooldatabase;
+
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Runner {
+	
+	public static void main(String args[]) {
+		
+		ArrayList<Account> studentDatabase = new ArrayList<>();
+		System.out.println(
+				"\nWelcome!\n\n"
+				+ "Create a student database via...\n\n"
+				+ "\t[1] ==> CONSOLE\n"
+				+ "\t[2] ==> TEXTFILE\n"
+				+ "\t[3] ==> CSV FILE\n");
+		
+		Scanner sc = new Scanner(System.in);
+		boolean invalidInput;
+		int selection;
+		do {
+			String input = sc.nextLine();
+			
+			selection = Integer.parseInt(input);
+			invalidInput = true;
+			if (selection == 1 || selection == 2 || selection == 3) invalidInput = false;
+			
+			if (input.length() > 1 || invalidInput) {
+				System.out.println("Please enter a valid selection.");
+			}
+			
+		} while (invalidInput);
+		
+		switch (selection) {
+		case 1:
+			ConsoleInput ci = new ConsoleInput();
+			ci.fileType();
+			studentDatabase = ci.read("");
+			break;
+		case 2:
+			TextFileReader tfr = new TextFileReader();
+			tfr.fileType();
+			studentDatabase = tfr.read("");
+			break;
+		case 3:
+			CSVReader csvr = new CSVReader();
+			csvr.fileType();
+			studentDatabase = csvr.read("");
+			break;
+		default:
+			System.out.println("Invalid input.");
+		}
+		
+		sc.close();
+	}
+}
