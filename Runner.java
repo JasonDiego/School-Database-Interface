6db9ee3b8e5d337bf8e1de89c7ec44211e718519
JasonAg <jason.aguilera.s@gmail.com>
2020-03-25 11:31:45 -0400

    delegated input reading to Runner.takeInput(), added MySQL option

--- Runner.java
+++ Runner.java
@@ -14,23 +14,10 @@ public class Runner {
 				+ "Create a student database via...\n\n"
 				+ "\t[1] ==> CONSOLE\n"
 				+ "\t[2] ==> TEXTFILE\n"
-				+ "\t[3] ==> CSV FILE\n");
+				+ "\t[3] ==> CSV FILE\n"
+				+ "\t[4] ==> MYSQL SERVER");
 		
-		Scanner sc = new Scanner(System.in);
-		boolean invalidInput;
-		int selection;
-		do {
-			String input = sc.nextLine();
-			
-			selection = Integer.parseInt(input);
-			invalidInput = true;
-			if (selection == 1 || selection == 2 || selection == 3) invalidInput = false;
-			
-			if (input.length() > 1 || invalidInput) {
-				System.out.println("Please enter a valid selection.");
-			}
-			
-		} while (invalidInput);
+		int selection = Runner.takeInput();
 		
 		switch (selection) {
 		case 1:
@@ -48,10 +35,36 @@ public class Runner {
 			csvr.fileType();
 			studentDatabase = csvr.read("");
 			break;
+		case 4:
+			
+			break;
 		default:
 			System.out.println("Invalid input.");
 		}
 		
+		
+	}
+	
+	private static int takeInput() {
+		
+		Scanner sc = new Scanner(System.in);
+		boolean invalidInput;
+		int selection;
+		
+		do {
+			String input = sc.nextLine();
+			
+			selection = Integer.parseInt(input);
+			invalidInput = true;
+			if (selection == 1 || selection == 2 || selection == 3) invalidInput = false;
+			
+			if (input.length() > 1 || invalidInput) {
+				System.out.println("Please enter a valid selection.");
+			}
+			
+		} while (invalidInput);
+		
 		sc.close();
+		return selection;
 	}
 }
